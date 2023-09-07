3e1f6bf5b6834017144484dd647ce89f6e16fe47
Branden Wood <brandenek03912@hotmail.com>
2023-09-06 22:29:54 -0400

    Add free cam module

--- BlasII.DebugMod/DebugMod.cs
+++ BlasII.DebugMod/DebugMod.cs
@@ -1,4 +1,5 @@
-﻿using BlasII.DebugMod.Hitboxes;
+﻿using BlasII.DebugMod.FreeCam;
+using BlasII.DebugMod.Hitboxes;
 using BlasII.ModdingAPI;
 
 namespace BlasII.DebugMod
@@ -8,25 +9,30 @@ namespace BlasII.DebugMod
         public DebugMod() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
         public HitboxViewer HitboxViewer { get; private set; }
+        public CameraMover CameraMover { get; private set; }
 
         protected override void OnInitialize()
         {
             HitboxViewer = new HitboxViewer(new HitboxConfig(true, false, 1f));
+            CameraMover = new CameraMover(new CameraConfig(0.1f, 2.5f));
         }
 
         protected override void OnSceneLoaded(string sceneName)
         {
             HitboxViewer.SceneLoaded();
+            CameraMover.SceneLoaded();
         }
 
         protected override void OnSceneUnloaded(string sceneName)
         {
             HitboxViewer.SceneUnloaded();
+            CameraMover.SceneUnloaded();
         }
 
         protected override void OnUpdate()
         {
             HitboxViewer.Update();
+            CameraMover.Update();
         }
     }
 }
