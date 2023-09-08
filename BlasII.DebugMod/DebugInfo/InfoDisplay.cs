2f3dd4d4e0059f91cde3f4c976a23e6f509040a3
Branden Wood <brandenek03912@hotmail.com>
2023-09-08 12:41:52 -0400

    Display scene and position

--- BlasII.DebugMod/DebugInfo/InfoDisplay.cs
+++ BlasII.DebugMod/DebugInfo/InfoDisplay.cs
@@ -1,27 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlasII.ModdingAPI.UI;
+using Il2CppTGK.Game;
+using Il2CppTMPro;
+using System;
+using UnityEngine;
 
 namespace BlasII.DebugMod.DebugInfo
 {
     public class InfoDisplay
     {
+        private bool _showInfo = false;
+
+        private TextMeshProUGUI _infoText;
 
         public void SceneLoaded()
         {
-
+            if (_showInfo)
+                SetTextVisibility(true);
         }
 
         public void SceneUnloaded()
         {
-
+            if (_showInfo)
+                SetTextVisibility(false);
         }
 
         public void Update()
         {
+            if (Input.GetKeyDown(KeyCode.F1) && Main.DebugMod.LoadStatus.GameSceneLoaded)
+            {
+                _showInfo = !_showInfo;
+                SetTextVisibility(_showInfo);
+            }
 
+            if (_showInfo)
+            {
+                UpdateText();
+            }
+        }
+
+        private void UpdateText()
+        {
+            string currentScene = Main.DebugMod.LoadStatus.CurrentScene;
+            Vector2 playerPosition = CoreCache.PlayerSpawn.PlayerInstance.transform.position;
+
+            _infoText.text = $"Scene: {currentScene}\nPosition: {Math.Round(playerPosition.x, 1)}, {Math.Round(playerPosition.y, 1)}";
+        }
+
+        private void SetTextVisibility(bool visible)
+        {
+            if (_infoText == null)
+                CreateText();
+
+            _infoText.gameObject.SetActive(visible);
+        }
+
+
+        private void CreateText()
+        {
+            _infoText = UIModder.CreateRect("Info Display", UIModder.Parents.GameLogic)
+                .SetXRange(0, 0).SetYRange(1, 1).SetPivot(0, 1).SetPosition(20, -235).SetSize(400, 200).AddText()
+                .SetFontSize(40).SetAlignment(TextAlignmentOptions.TopLeft);
         }
     }
 }
