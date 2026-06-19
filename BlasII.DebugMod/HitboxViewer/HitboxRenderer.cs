5e07504a023ffd48c735941477659fe537e85760
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-06-19 11:25:22 -0400

    hitbox: restore regular functionality

--- BlasII.DebugMod/HitboxViewer/HitboxRenderer.cs
+++ BlasII.DebugMod/HitboxViewer/HitboxRenderer.cs
@@ -15,10 +15,16 @@ internal class HitboxRenderer
     private Collider2D[] _cachedColliders = null;
     private bool _isShowing = false;
 
+    private int _segments;
+    private float _angleStep;
+
     public HitboxRenderer(HitboxViewerSettings settings)
     {
         _settings = settings;
         CacheLineMaterial();
+
+        _segments = settings.ArcSegments;
+        _angleStep = 2 * Mathf.PI / _segments;
     }
 
     public void UpdateColliders(Collider2D[] colliders)
@@ -66,7 +72,7 @@ internal class HitboxRenderer
         if (!_isShowing || cam != _camera || _cachedColliders == null)
             return;
 
-        ModLog.Info("Rendering for " + cam.name);
+        //ModLog.Info("Rendering for " + cam.name);
 
         _material.SetPass(0);
         CacheCameraBounds();
@@ -76,10 +82,196 @@ internal class HitboxRenderer
 
         // ...
 
+        foreach (var info in _cachedColliders.Select(CalculateInfo).OrderBy(x => x.Type))
+        {
+            if (!info.IsVisible)
+                continue;
+
+            GL.Color(TypeToColor(info.Type));
+
+            switch (info.Collider.GetIl2CppType().Name)
+            {
+                case "BoxCollider2D":
+                    RenderBox(info.Collider.Cast<BoxCollider2D>());
+                    break;
+                case "CircleCollider2D":
+                    RenderCircle(info.Collider.Cast<CircleCollider2D>());
+                    break;
+                case "CapsuleCollider2D":
+                    RenderCapsule(info.Collider.Cast<CapsuleCollider2D>());
+                    break;
+                case "PolygonCollider2D":
+                    RenderPolygon(info.Collider.Cast<PolygonCollider2D>());
+                    break;
+                default:
+                    break;
+            }
+        }
+
         GL.Color(Color.red);
         GL.Vertex(new Vector3(0, 0, 1));
         GL.Vertex(new Vector3(1, 1, 1));
 
         GL.End();
+    }
+
+    void RenderBox(BoxCollider2D collider)
+    {
+        Vector2 halfSize = collider.size / 2f;
+        var topLeft = CalculateViewport(collider, new Vector2(-halfSize.x, halfSize.y));
+        var topRight = CalculateViewport(collider, new Vector2(halfSize.x, halfSize.y));
+        var bottomRight = CalculateViewport(collider, new Vector2(halfSize.x, -halfSize.y));
+        var bottomLeft = CalculateViewport(collider, new Vector2(-halfSize.x, -halfSize.y));
+
+        GL.Vertex(topLeft);
+        GL.Vertex(topRight);
+
+        GL.Vertex(topRight);
+        GL.Vertex(bottomRight);
+
+        GL.Vertex(bottomRight);
+        GL.Vertex(bottomLeft);
+
+        GL.Vertex(bottomLeft);
+        GL.Vertex(topLeft);
+    }
+
+    void RenderCircle(CircleCollider2D collider)
+    {
+        float radius = collider.radius;
+
+        Vector3 start = CalculateViewport(collider, new Vector2(radius, 0));
+        GL.Vertex(start);
+
+        for (int i = 1; i < _segments; i++)
+        {
+            float angle = i * _angleStep;
+            float x = Mathf.Cos(angle) * radius;
+            float y = Mathf.Sin(angle) * radius;
+
+            Vector3 point = CalculateViewport(collider, new Vector2(x, y));
+            GL.Vertex(point);
+            GL.Vertex(point);
+        }
+
+        GL.Vertex(start);
+    }
+
+    void RenderCapsule(CapsuleCollider2D collider)
+    {
+        float radius = collider.size.x / 2;
+        float height = collider.size.y / 2;
+
+        Vector3 start = CalculateViewport(collider, new Vector2(0, height));
+        GL.Vertex(start);
+
+        for (int i = 1; i < _segments; i++)
+        {
+            float angle = i * _angleStep;
+            float x = Mathf.Sin(angle) * radius;
+            float y = Mathf.Cos(angle) * height;
+
+            Vector3 point = CalculateViewport(collider, new Vector2(x, y));
+            GL.Vertex(point);
+            GL.Vertex(point);
+        }
+
+        GL.Vertex(start);
+    }
+
+    void RenderPolygon(PolygonCollider2D collider)
+    {
+        if (collider.pathCount == 0)
+            return;
+
+        Vector2[] points = collider.GetPath(0);
+
+        if (points.Length < 3)
+            return;
+
+        Vector3 start = CalculateViewport(collider, points[0]);
+        GL.Vertex(start);
+
+        for (int i = 1; i < points.Length; i++)
+        {
+            Vector3 point = CalculateViewport(collider, points[i]);
+
+            GL.Vertex(point);
+            GL.Vertex(point);
+        }
+
+        GL.Vertex(start);
+    }
+
+    private Vector3 CalculateViewport(Collider2D collider, Vector3 point)
+    {
+        Transform t = collider.transform;
+        Vector2 offset = collider.offset;
+        Vector3 position = t.position;
+        Vector3 scale = t.lossyScale;
+
+        // Apply offset
+        point.x += offset.x;
+        point.y += offset.y;
+
+        // Apply rotation
+        point = t.rotation * point;
+
+        // Apply scale
+        point.x *= scale.x;
+        point.y *= scale.y;
+
+        // Convert to world
+        point.x += position.x;
+        point.y += position.y;
+
+        point = _camera.WorldToViewportPoint(point);
+        point.z = 1;
+        return point;
+    }
+
+    private Color TypeToColor(HitboxType hitboxType)
+    {
+        string color = hitboxType switch
+        {
+            HitboxType.Inactive => _settings.InactiveColor,
+            HitboxType.Hazard => _settings.HazardColor,
+            HitboxType.Damageable => _settings.DamageableColor,
+            HitboxType.Player => _settings.PlayerColor,
+            HitboxType.Sensor => _settings.SensorColor,
+            HitboxType.Enemy => _settings.EnemyColor,
+            HitboxType.Interactable => _settings.InteractableColor,
+            HitboxType.Trigger => _settings.TriggerColor,
+            HitboxType.Geometry => _settings.GeometryColor,
+            HitboxType.Other => _settings.OtherColor,
+            _ => throw new System.Exception("A valid type should be calculated before now!"),
+        };
+
+        return ColorUtility.TryParseHtmlString(color, out Color c) ? c : Color.white;
+    }
+
+    private HitboxInfo CalculateInfo(Collider2D collider)
+    {
+        // Verify collider still exists
+        if (collider == null)
+            return new HitboxInfo(collider, HitboxType.Invalid, false);
+
+        // Verify collider is in camera bounds
+        if (collider.bounds.min.x > _camBounds.max.x || collider.bounds.min.y > _camBounds.max.y ||
+            collider.bounds.max.x < _camBounds.min.x || collider.bounds.max.y < _camBounds.min.y)
+            return new HitboxInfo(collider, HitboxType.Invalid, false);
+
+        // Verify collider is a valid size
+        Vector2 size = collider.bounds.extents * 2;
+        if (collider.enabled && (size.x < _settings.MinSize || size.x > _settings.MaxSize || size.y < _settings.MinSize || size.y > _settings.MaxSize))
+            return new HitboxInfo(collider, HitboxType.Invalid, false);
+
+        HitboxType type = collider.GetHitboxType();
+
+        // Verify collider is toggled on
+        if (!Main.DebugMod.HitboxModule.ToggledHitboxes[type])
+            return new HitboxInfo(collider, type, false);
+
+        return new HitboxInfo(collider, type, true);
     }
 }
