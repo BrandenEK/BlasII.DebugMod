bc899cb011e5740ae298a5cef49985ecbbcf28c7
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-07-13 18:36:26 -0400

    info: clean up patches

--- BlasII.DebugMod/InfoDisplay/InfoPatches.cs
+++ BlasII.DebugMod/InfoDisplay/InfoPatches.cs
@@ -1,33 +1,18 @@
-﻿using BlasII.ModdingAPI;
-using HarmonyLib;
-using Il2CppLightbug.Kinematic2D.Implementation;
-using Il2CppTGK.Game.Achievements;
-using Il2CppTGK.Game.Achievements.Components;
+﻿using HarmonyLib;
 using Il2CppTGK.Game.Components;
-using Il2CppTGK.Game.Components.Attack;
 using Il2CppTGK.Game.Components.Attack.Data;
-using Il2CppTGK.Game.Components.Defense;
-using Il2CppTGK.Game.Components.Health;
-using Il2CppTGK.Game.Systems;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
 
 namespace BlasII.DebugMod.InfoDisplay;
 
 [HarmonyPatch(typeof(AttackReceivedBlackboard), nameof(AttackReceivedBlackboard.OnAttackReceived))]
-class x_Patch
+class AttackReceivedBlackboard_OnAttackReceived_Patch
 {
-    public static void Postfix(AttackReceivedBlackboard __instance, AttackInfo attackInfo)
+    public static void Postfix(AttackInfo attackInfo)
     {
-        ModLog.Info("Received hit for  blackboard");
+        //ModLog.Info("Received hit from AttackReceivedBlackboard");
 
         string id = attackInfo.attackID?.name;
         int damage = attackInfo.accumulatedDamage;
-        ModLog.Error(damage);
 
         var data = attackInfo.attack;
 
@@ -40,7 +25,6 @@ class x_Patch
             }
         }
         physical = physical.Replace(" Attack", string.Empty).Replace("Damage", string.Empty);
-        ModLog.Warn(physical);
 
         string elemental = string.Empty;
         if (data.elementalAttackSources != null)
@@ -51,34 +35,21 @@ class x_Patch
             }
         }
         elemental = elemental.Replace(" Attack", string.Empty).Replace("Damage", string.Empty);
-        ModLog.Warn(elemental);
 
         var result = new AttackData(id, damage, physical, elemental);
         Main.DebugMod.InfoModule.OnOffense(result);
     }
 }
 
-// Crashes
-//[HarmonyPatch(typeof(HitFeedback), nameof(HitFeedback.AttackReceived))]
-//class u
-//{
-//    public static void Postfix(HitFeedback __instance, AttackInfo attack)
-//    {
-//        ModLog.Error(__instance.name);
-//        ModLog.Info(attack.accumulatedDamage);
-//    }
-//}
-
 [HarmonyPatch(typeof(PlayerDeathEventBroadcaster), nameof(PlayerDeathEventBroadcaster.OnAttackReceived))]
-class v
+class PlayerDeathEventBroadcaster_OnAttackReceived_Patch
 {
     public static void Postfix(AttackInfo attackInfo)
     {
-        ModLog.Info("Received hit for playerdeath");
+        //ModLog.Info("Received hit from PlayerDeathEventBroadcaster");
 
         string id = attackInfo.attackID?.name;
         int damage = attackInfo.accumulatedDamage;
-        ModLog.Error(damage);
 
         var data = attackInfo.attack;
 
@@ -91,7 +62,6 @@ class v
             }
         }
         physical = physical.Replace(" Attack", string.Empty).Replace("Damage", string.Empty);
-        ModLog.Warn(physical);
 
         string elemental = string.Empty;
         if (data.elementalAttackSources != null)
@@ -102,98 +72,8 @@ class v
             }
         }
         elemental = elemental.Replace(" Attack", string.Empty).Replace("Damage", string.Empty);
-        ModLog.Warn(elemental);
 
         var result = new AttackData(id, damage, physical, elemental);
         Main.DebugMod.InfoModule.OnDefense(result);
     }
 }
-
-//[HarmonyPatch(typeof(AttackAbility), nameof(AttackAbility.AttackHit))]
-[HarmonyPatch(typeof(AttackResolutionSystem), nameof(AttackResolutionSystem.BuildAttackInfo))]
-class y_Patch
-{
-    public static void Postfix(AttackResolutionSystem __instance, CharacterController2D owner)
-    {
-        //ModLog.Info("Received hit for " + owner.gameObject.name);
-        //var attackInfo = __instance.attackInfo;
-
-        //string id = attackInfo.attackID?.name;
-        //int damage = attackInfo.accumulatedDamage;
-        //ModLog.Error(damage);
-
-        //var data = attackInfo.attack;
-
-        //string physical = string.Empty;
-        //if (data.physicalAttackSources != null)
-        //{
-        //    for (int i = 0; i < data.physicalAttackSources.Count; i++)
-        //    {
-        //        physical += $"{data.physicalAttackSources[i].attackType?.name} ";
-        //    }
-        //}
-        //physical = physical.Replace(" Attack", string.Empty).Replace("Damage", string.Empty);
-        //ModLog.Warn(physical);
-
-        //string elemental = string.Empty;
-        //if (data.elementalAttackSources != null)
-        //{
-        //    for (int i = 0; i < data.elementalAttackSources.Count; i++)
-        //    {
-        //        elemental += $"{data.elementalAttackSources[i].attackType?.name} ";
-        //    }
-        //}
-        //elemental = elemental.Replace(" Attack", string.Empty).Replace("Damage", string.Empty);
-        //ModLog.Warn(elemental);
-
-        //var result = new AttackData(id, damage, physical, elemental);
-        //if (owner.gameObject.name == "#Main Player")
-        //    Main.DebugMod.InfoModule.OnDefense(result);
-        //else
-        //    Main.DebugMod.InfoModule.OnOffense(result);
-    }
-}
-
-//[HarmonyPatch(typeof(AttackReceiverComponent), nameof(AttackReceiverComponent.OnAttackReceive), typeof(AttackHit), typeof(Collision2D))]
-//class y_Patch
-//{
-//    public static void Postfix(AttackReceiverComponent __instance)
-//    {
-//        ModLog.Info("Received hit for " + __instance.gameObject.name);
-//        var attack = __instance.GetSystem().attackInfo;
-
-//        string id = attack.attackID?.name;
-//        int damage = attack.accumulatedDamage;
-//        ModLog.Error(damage);
-
-//        var data = attack.attack;
-
-//        string physical = string.Empty;
-//        if (data.physicalAttackSources != null)
-//        {
-//            for (int i = 0; i < data.physicalAttackSources.Count; i++)
-//            {
-//                physical += $"{data.physicalAttackSources[i].attackType?.name} ";
-//            }
-//        }
-//        physical = physical.Replace(" Attack", string.Empty).Replace("Damage", string.Empty);
-//        ModLog.Warn(physical);
-
-//        string elemental = string.Empty;
-//        if (data.elementalAttackSources != null)
-//        {
-//            for (int i = 0; i < data.elementalAttackSources.Count; i++)
-//            {
-//                elemental += $"{data.elementalAttackSources[i].attackType?.name} ";
-//            }
-//        }
-//        elemental = elemental.Replace(" Attack", string.Empty).Replace("Damage", string.Empty);
-//        ModLog.Warn(elemental);
-
-//        var result = new AttackData(id, damage, physical, elemental);
-//        if (__instance.gameObject.name == "#Main Player")
-//            Main.DebugMod.InfoModule.OnDefense(result);
-//        else
-//            Main.DebugMod.InfoModule.OnOffense(result);
-//    }
-//}
